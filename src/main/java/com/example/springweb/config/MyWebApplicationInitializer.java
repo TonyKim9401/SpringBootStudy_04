3fac453fd4159b39a32fe7ae5c24b20890c17d48
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2022-10-05 23:57:27 +0900

    Oct.05.2022

--- src/main/java/com/example/springweb/config/MyWebApplicationInitializer.java
+++ src/main/java/com/example/springweb/config/MyWebApplicationInitializer.java
@@ -1,16 +1,40 @@
 package com.example.springweb.config;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class MyWebApplicationInitializer implements WebApplicationInitializer {
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        return new Filter[] {encodingFilter};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class}; // 스프링의 설정 파일을 리턴시킴
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; // 모든 요청을 DispatcherServlet으로 보낸다.
+    }
+
+    /*    @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         System.out.println("on start up!!!");
 
@@ -21,5 +45,5 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
         ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", servlet);
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
-    }
+    }*/
 }
